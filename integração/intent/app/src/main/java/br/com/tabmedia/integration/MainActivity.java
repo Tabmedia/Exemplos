a0046227e52bc1804c03c598dbf1ee07f05c8013
vncmaciel <vnc.maciel@gmail.com>
2017-03-09 18:02:17 -0300

    integration via intent example

---
+++ integração/intent/app/src/main/java/br/com/tabmedia/integration/MainActivity.java
@@ -0,0 +1,60 @@
+package br.com.tabmedia.integration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText crm;
+    private EditText crmUf;
+    private EditText doctor;
+    private EditText products;
+    private EditText speciality;
+    private EditText visit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        crm = (EditText) findViewById(R.id.crm);
+        crmUf = (EditText) findViewById(R.id.crmuf);
+        doctor = (EditText) findViewById(R.id.doctor);
+        products = (EditText) findViewById(R.id.products);
+        speciality = (EditText) findViewById(R.id.speciality);
+        visit = (EditText) findViewById(R.id.visit);
+
+        Button btOpenTabmedia = (Button) findViewById(R.id.bt_open_tabmedia);
+        btOpenTabmedia.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        intent.setAction("br.com.tabmedia.openTabmedia");
+
+        intent.putExtra("name", getValue(doctor));
+        intent.putExtra("crm", getValue(crm));
+        intent.putExtra("crmUF", getValue(crmUf));
+        intent.putExtra("specialityName", getValue(speciality));
+        intent.putExtra("products", getValue(products));
+        intent.putExtra("presentationNumber", Integer.valueOf(getValue(visit)));
+
+        sendBroadcast(intent);
+    }
+
+    private String getValue(EditText editText) {
+        if (editText.equals(visit)) {
+            return (editText.getText().length() > 0) ? editText.getText().toString() : "1";
+        } else {
+            return (editText.getText().length() > 0) ? editText.getText().toString() : null;
+        }
+    }
+}
